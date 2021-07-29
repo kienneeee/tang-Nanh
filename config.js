ef5b3c4cff114e16d9a6ccaa651a576ce3f68458
kienneeee <88121944+kienneeee@users.noreply.github.com>
2021-07-29 09:00:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    introTitle: 'Nanh iu à!',
+    introDesc: `b_autiful đẹp hơn khi có e`,
+    btnIntro: 'ỏ~~~',
+    title: 'Phải chăng Nanh đã yêu Kiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Sao biết hay thế <33',
+    btnNo: 'Mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Nanh lại yêu Kiên <3',
+    btnReply: 'Gửi cho Kiên <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Kiên biết mà 🥰. Yêu Nanh nhất 😘😘',
+    messDesc: 'Sinh nhật vui vẻ nha công chúa ❤.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'http://fb.com' https://www.facebook.com/profile.php?id=100066933925753
